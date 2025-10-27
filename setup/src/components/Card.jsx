c31fc84d22a3cd7173fee4a1c4bc19349b6900f5
GunjanRathore0011 <your_personal_github_email@example.com>
2025-10-27 17:39:15 +0530

    djsdjfosd

--- setup/src/components/Card.jsx
+++ setup/src/components/Card.jsx
@@ -2,11 +2,44 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({ title = 'Card Title', children = 'This is an example card. Replace this text with anything you like.' }) => {
+	function clickHandler(){
+		alert("Clicked the tag..");
+	}
 	return (
-		<div className="card" role="article" aria-label={title}>
-			It is the card component 
-			changes are made here to see the git status and git diff working properly
+		<div className='card-container'>
+			<div className="card" role="article" aria-label={title}>
+				It is the card component
+				changes are made here to see the git status and git diff working properly
+			</div>
+			<button className='btn'> Sign in</button>
+
+			<label>
+				<input type="checkbox" /> Subscribe
+			</label>
+			<br />
+			<br></br>
+
+			<form>
+				<label>Enter Name: 
+				<input type="text" placeholder='Enter your name' />
+				</label>
+	
+
+			</form>
+			<br></br>
+
+			<button>Submit</button>
+			<br />
+			<br></br>
+			<a href='https://javascript.info/'  target='_blank' onClick={clickHandler}> Learn Javascript</a>
+
+
+			<br />
+			<br></br>
+			<input type='file' id='fileInput'></input>
+
 		</div>
+
 	);
 };
 
